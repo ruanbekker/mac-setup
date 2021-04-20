3db1555bf84cca87b0139433fa3e9b7558093836
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-04-20 08:46:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,19 +8,19 @@ brew install cask
 brew install --cask iterm2
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install --appdir="/Applications" awscli@2
+brew install --appdir="/Applications" jq
+brew install --appdir="/Applications" wget
+brew install --appdir="/Applications" go
+brew install --appdir="/Applications" kubernetes-cli
+brew install --cask --appdir="/Applications" multipass
 brew install --cask --appdir="/Applications" virtualbox
 brew install --cask --appdir="/Applications" vagrant
 brew install --cask --appdir="/Applications" visual-studio-code
-brew install --appdir="/Applications" jq
-brew install --appdir="/Applications" wget
 brew install --build-from-source fzf
-brew install --appdir="/Applications" kubernetes-cli
 
 /usr/local/opt/fzf/install
 source ~/.zshrc
-
-brew install --appdir="/Applications" go
-brew install --cask multipass
 
 brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
